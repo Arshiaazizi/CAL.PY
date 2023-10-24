b0e1864ae825cf0f1fff9b914fa90e26baf5c635
Arshiaazizi <103750705+Arshiaazizi@users.noreply.github.com>
2023-10-24 17:03:44 +0330

    Add files via upload

---
+++ calculatortask3.py
@@ -0,0 +1,52 @@
+import math
+
+op = input("enter op(+, /, *, -, sin, cos, tan, cot, factorial, sqrt):")
+
+if op == "/":
+    a = float(input("Enter a:"))
+    b = float(input("Enter b:"))
+    if b == 0:
+        r = "Error"
+    else:
+        r = a / b
+
+if op == "*":
+    a = float(input("Enter a:"))
+    b = float(input("Enter b:"))
+    r = a * b
+
+if op == "+":
+    a = float(input("Enter a:"))
+    b = float(input("Enter b:"))
+    r = a + b
+
+if op == "-":
+    a = float(input("Enter a:"))
+    b = float(input("Enter b:"))
+    r = a - b
+
+if op == "cos":
+    a = float(input("Enter a:"))
+    r = (math.cos(math.radians(a)))
+
+if op == "cot":
+    a = float(input("Enter a:"))
+    r = 1/(math.tan(math.radians(a)))
+
+if op == "sin":
+    a = float(input("Enter a:"))
+    r = (math.sin(math.radians(a)))
+
+if op == "sqrt":
+    a = float(input("Enter a:"))
+    r = (math.sqrt(a))
+
+if op == "tan":
+    a = float(input("Enter a:"))
+    r = (math.tan(math.radians(a)))
+
+if op == "factorial":
+    a = int(input("Enter a:"))
+    r = (math.factorial(a))
+
+print(r)
